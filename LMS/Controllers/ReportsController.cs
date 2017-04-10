5fa5b1d7c5709b5d699aacf76cbd11f011052fe6
Marmik Shrestha <memadrid7@gmail.com>
2017-04-10 15:47:31 +0545

    Controller Changes

--- LMS/Controllers/ReportsController.cs
+++ LMS/Controllers/ReportsController.cs
@@ -1,140 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using PagedList;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using LMS.Models;
 
 namespace LMS.Controllers
 {
     public class ReportsController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
-
-        // GET: OldBooks
-        public ActionResult Index(int? page)
+        // GET: Reports
+        public ActionResult OldBooks()
         {
-			int pageSize = 10;
-			int pageNumber = (page ?? 1);
-            var books = db.Books.Include(b => b.Press).Include(b => b.Publisher);
-            return View(books.OrderByDescending(m => m.LastUpdated).ToPagedList(pageNumber, pageSize));
-        }
-
-        // GET: OldBooks/Details/5
-        public ActionResult Details(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Book book = db.Books.Find(id);
-            if (book == null)
-            {
-                return HttpNotFound();
-            }
-            return View(book);
-        }
-
-        // GET: OldBooks/Create
-        public ActionResult Create()
-        {
-            ViewBag.PressID = new SelectList(db.Press, "ID", "Name");
-            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name");
             return View();
         }
 
-        // POST: OldBooks/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Name,ISBN,PublisherID,PressID,PublishedDate,Charge,PenaltyCharge,LastUpdatedBy,LastUpdated")] Book book)
+        public ActionResult InactiveBooks()
         {
-            if (ModelState.IsValid)
-            {
-                book.ID = Guid.NewGuid();
-                db.Books.Add(book);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.PressID = new SelectList(db.Press, "ID", "Name", book.PressID);
-            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name", book.PublisherID);
-            return View(book);
+            return View();
         }
 
-        // GET: OldBooks/Edit/5
-        public ActionResult Edit(Guid? id)
+        public ActionResult InactiveMembers()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Book book = db.Books.Find(id);
-            if (book == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.PressID = new SelectList(db.Press, "ID", "Name", book.PressID);
-            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name", book.PublisherID);
-            return View(book);
+            return View();
         }
 
-        // POST: OldBooks/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,ISBN,PublisherID,PressID,PublishedDate,Charge,PenaltyCharge,LastUpdatedBy,LastUpdated")] Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(book).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.PressID = new SelectList(db.Press, "ID", "Name", book.PressID);
-            ViewBag.PublisherID = new SelectList(db.Publishers, "ID", "Name", book.PublisherID);
-            return View(book);
-        }
 
-        // GET: OldBooks/Delete/5
-        public ActionResult Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Book book = db.Books.Find(id);
-            if (book == null)
-            {
-                return HttpNotFound();
-            }
-            return View(book);
-        }
-
-        // POST: OldBooks/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(Guid id)
-        {
-            Book book = db.Books.Find(id);
-            db.Books.Remove(book);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
